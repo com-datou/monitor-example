1809413f0fdd90a31fa5fb7ec6a1062cebde576a
michael_sun <michael.sun@dyuanzi.com>
2019-09-27 10:05:07 +0800

    add eureka support

--- src/main/java/com/datou/MonitorApplication.java
+++ src/main/java/com/datou/MonitorApplication.java
@@ -3,12 +3,15 @@ package com.datou;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableAdminServer
+@EnableEurekaClient
 public class MonitorApplication {
 
 	public static void main(String[] args) {
