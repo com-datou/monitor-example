4e892a60e3db191ad5fe0df00fc151373dcc85ca
michael_sun <michael.sun@dyuanzi.com>
2019-10-10 18:03:58 +0800

    change for : port change

--- src/main/java/com/datou/MonitorApplication.java
+++ src/main/java/com/datou/MonitorApplication.java
@@ -3,7 +3,6 @@ package com.datou;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
